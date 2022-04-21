09ca25fcb7d9bf2d9ff1fe48378f3e12d95b75a9
Emmanuel Azocar <azocarmel@gmail.com>
2022-04-20 23:30:24 -0400

    feat: added graphql

---
+++ services/GraphQl/mutations/events.mutations.ts
@@ -0,0 +1,11 @@
+import { gql } from '@apollo/client'
+
+const CREATE_EVENT = gql`
+  mutation CreateEvent($title: String!, $description: String!, $date: String!, $type: String!) {
+    insert_events(objects: { date: $date, title: $title, description: $description, type: $type }) {
+      affected_rows
+    }
+  }
+`
+
+export { CREATE_EVENT }
