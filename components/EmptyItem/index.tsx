f5eb97a3a5e5a23762943ad121a6275c81d923eb
Israel-Laguan <israellaguan@gmail.com>
2022-06-25 04:30:13 -0500

    feat: add empty case components

---
+++ components/EmptyItem/index.tsx
@@ -0,0 +1,5 @@
+import { type FC } from 'react'
+
+const EmptyItem: FC<{ text: string }> = ({ text }) => <h3>{text}</h3>
+
+export default EmptyItem
---
+++ components/EmptyList/index.tsx
@@ -0,0 +1,5 @@
+import { type FC } from 'react'
+
+const EmptyList: FC<{ text: string }> = ({ text }) => <h3>{text}</h3>
+
+export default EmptyList
