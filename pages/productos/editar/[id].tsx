0367a9e856c43313c0b748ce288569c06e4eadb3
Israel-Laguan <israellaguan@gmail.com>
2022-06-26 02:23:54 -0500

    fix: use UpdateFormContainer for edit product

--- pages/productos/editar/[id].tsx
+++ pages/productos/editar/[id].tsx
@@ -2,8 +2,10 @@ import { useMutation } from '@apollo/client'
 import { NextPage } from 'next'
 import { useRouter } from 'next/router'
 
+import UpdateFormContainer from '@/components/UpdateForm'
 import useLogger from '@/hooks/useLogger'
 import { UPDATE_PRODUCT } from '@/services/GraphQL/products/mutations'
+import { PRODUCT_BY_ID } from '@/services/GraphQL/products/queries'
 import { ProductEditable } from '@/services/GraphQL/products/types'
 import EditProductLayout from '@/views/Products/Edit'
 
@@ -20,7 +22,14 @@ const EditProduct: NextPage = () => {
 
   if (mutationError) logError(mutationError, 'pages/productos/[id].tsx', 'Error al actualizar el producto')
 
-  return <EditProductLayout onSubmit={submitHandler} loading={loading} />
+  return (
+    <UpdateFormContainer
+      currentDataQuery={PRODUCT_BY_ID}
+      submitHandler={submitHandler}
+      isSubmitLoading={loading}
+      UpdateForm={EditProductLayout}
+    />
+  )
 }
 
 export default EditProduct
