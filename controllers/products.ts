7a519929b59477d09838640a9a1eef9edf6faab7
Emmanuel Azocar <azocarmel@gmail.com>
2022-08-17 23:10:34 -0400

    feat: added products endpoints

--- controllers/products.ts
+++ controllers/products.ts
@@ -1,13 +1,32 @@
+import client from '@/services/GraphQL/client'
+import { CREATE_PRODUCT } from '@/services/GraphQL/products/mutations'
+import { PRODUCTS } from '@/services/GraphQL/products/queries'
 import formidable from 'formidable'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 import { addObject } from '../services/AWS/s3'
 
+export const getProducts = async (_: NextApiRequest, res: NextApiResponse) => {
+  const { data } = await client.query({
+    query: PRODUCTS,
+  })
+
+  res.json(data)
+}
+
 export const createProduct = (req: NextApiRequest, res: NextApiResponse) => {
   const form = formidable()
-  form.parse(req, async (_err, _fields, files) => {
+  form.parse(req, async (_err, fields, files) => {
     const file = files.image as formidable.File
-    const { Location: imageURL } = await addObject(file)
-    res.json({ msg: 'File saved successfully', imageURL })
+    const { Location: imageUrl } = await addObject(file)
+
+    await client.mutate({
+      mutation: CREATE_PRODUCT,
+      variables: { ...fields, imageUrl },
+    })
+    res.json({
+      msg: 'Product created successfully',
+      data: { ...fields, imageUrl },
+    })
   })
 }
--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -1,11 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-import { createProduct } from '../../../controllers/products'
+import { createProduct, getProducts } from '../../../controllers/products'
 
 export const config = {
   api: { bodyParser: false },
 }
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') getProducts(req, res)
   if (req.method === 'POST') createProduct(req, res)
 }
