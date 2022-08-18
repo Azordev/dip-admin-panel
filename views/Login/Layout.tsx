ee89f1a7971539ff2505480e10f4fecd47747f15
Emmanuel Azócar <azocarmel@gmail.com>
2022-08-18 15:45:01 +0000

    fix: test

--- views/Login/Layout.tsx
+++ views/Login/Layout.tsx
@@ -11,7 +11,7 @@ import styles from './Login.module.scss'
 const LoginLayout: NextPage<LoginFormProps> = ({ onSubmit, loading }) => (
   <Centered>
     <div className={styles['login-container']}>
-      <h1>AWS Test Preview</h1>
+      <h1>AWS Test Preview New Domain</h1>
       <LogoDID className={styles.image} />
       <LoginForm onSubmit={onSubmit} loading={loading} />
     </div>
