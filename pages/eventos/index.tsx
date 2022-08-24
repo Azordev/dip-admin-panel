abc35d875dd7954523cd2e8f6646532b45861d6a
Emmanuel Azocar <azocarmel@gmail.com>
2022-08-24 14:37:38 -0400

    feat: get events from api

--- pages/eventos/index.tsx
+++ pages/eventos/index.tsx
@@ -1,30 +1,36 @@
-import { useQuery } from '@apollo/client'
-import { NextPage } from 'next'
+import axios from 'axios'
+import { GetServerSideProps, NextPage } from 'next'
 
 import EmptyList from '@/components/EmptyList'
 import ListHeader from '@/components/ListHeader'
-import Loading from '@/components/Loading'
-import useLogger from '@/hooks/useLogger'
-import { EVENTS } from '@/services/GraphQL/events/queries'
+import { Event } from '@/services/GraphQL/events/types'
 import EventsList from '@/views/Events/List'
 import ClientOnly from '@/views/Shared/ClientOnly'
 
-const Events: NextPage = () => {
-  const { data, loading, error: queryError } = useQuery(EVENTS)
-  const { error: logError } = useLogger()
+interface PageProps {
+  events: Event[]
+}
 
-  if (queryError) logError(queryError, 'pages/events/index.tsx', 'No se pudo obtener los eventos de la base de datos')
-
-  if (loading) return <Loading />
-  if (!data && data.events.length < 1) return <EmptyList text="No hay eventos" />
+const Events: NextPage<PageProps> = ({ events }) => {
+  if (!events || events.length < 1) return <EmptyList text="No hay eventos" />
   return (
     <ClientOnly>
       <>
         <ListHeader createText="Crear nuevo evento" createPath="/eventos/crear" />
-        <EventsList events={data.events} />
+        <EventsList events={events} />
       </>
     </ClientOnly>
   )
 }
 
 export default Events
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { data } = await axios.get('http://localhost:3000/api/events')
+
+  return {
+    props: {
+      events: data.events || [],
+    },
+  }
+}
