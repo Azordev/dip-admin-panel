5d2dcf289cf5aae6a53b2db4770a9e5c0a8ada98
leonel <leonelhcontreras@gmail.com>
2022-08-27 22:56:39 -0400

    Feat: Creation of the Create Product form

--- views/Products/Create/Form.tsx
+++ views/Products/Create/Form.tsx
@@ -13,15 +13,62 @@ const CreateProductForm: FC<MutableProductFormProps> = ({ onSubmit, loading }) =
   const buttonText = loading ? 'Enviando' : 'Enviar'
 
   return (
-    <form onSubmit={submitHandler}>
-      <input type="text" placeholder="name" {...register('name', { required: true })} />
-      {errors.name && <small className="text-red-500">{errors.name.message}</small>}
-      <input type="text" placeholder="description" {...register('description', { required: true })} />
+    <form className="form-product" onSubmit={submitHandler}>
+      <label className="text-size label" htmlFor="name">
+        Nombre del Producto
+      </label>
+      <input
+        className="input"
+        id="name"
+        type="text"
+        placeholder="Escriba el nombre del evento..."
+        {...register('name', { required: true })}
+      />
+
+      <label className="text-size label" htmlFor="basePriceSol">
+        Precio del producto
+      </label>
+
+      <div className="container-price">
+        <p className="price">S/.</p>
+        <input className="input" type="text" placeholder="00.00" {...register('basePriceSol', { required: true })} />
+        {errors.basePriceSol && <small className="text-red-500">{errors.basePriceSol.message}</small>}
+      </div>
+
+      <label className="text-size label" htmlFor="description">
+        Descripción del producto
+      </label>
+      <textarea
+        className="textarea"
+        id="description"
+        placeholder="Escribe aquí..."
+        {...register('description', { required: true })}
+      />
       {errors.description && <small className="text-red-500">{errors.description.message}</small>}
-      <input type="text" placeholder="image_url" {...register('imageUrl', { required: true })} />
+
+      <label className="text-size label">Añadir imagen del producto</label>
+      <label
+        className="image font-visby"
+        // {...register('imageUrl', { required: true })}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1}
+          stroke="currentColor"
+          // className="w-3 h-3"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+          />
+        </svg>
+        Añadir imagen
+      </label>
       {errors.imageUrl && <small className="text-red-500">{errors.imageUrl.message}</small>}
-      <input type="text" placeholder="base_price_sol" {...register('basePriceSol', { required: true })} />
-      {errors.basePriceSol && <small className="text-red-500">{errors.basePriceSol.message}</small>}
+
       <button type="submit">{buttonText}</button>
     </form>
   )
