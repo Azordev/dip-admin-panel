e587ee6d1f1907534844a89a54bba59b0ab2ef1c
leonel <leonelhcontreras@gmail.com>
2022-08-29 11:29:51 -0400

    fixed styles 3

--- views/Products/Create/Form.tsx
+++ views/Products/Create/Form.tsx
@@ -3,6 +3,8 @@ import { useForm } from 'react-hook-form'
 
 import { MutableProductFormProps, ProductEditable } from '@/services/GraphQL/products/types'
 
+import { ImageSVG } from './AddImageSVG'
+
 const CreateProductForm: FC<MutableProductFormProps> = ({ onSubmit, loading }) => {
   const {
     register,
@@ -56,20 +58,7 @@ const CreateProductForm: FC<MutableProductFormProps> = ({ onSubmit, loading }) =
         className="image font-visby"
         // {...register('imageUrl', { required: true })}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1}
-          stroke="currentColor"
-          // className="w-3 h-3"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
-          />
-        </svg>
+        {ImageSVG}
         Añadir imagen
       </label>
       {errors.imageUrl && <small className="text-red-500">{errors.imageUrl.message}</small>}
