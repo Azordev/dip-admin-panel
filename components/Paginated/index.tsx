f56f05bc04fb683a3f582140331d33eb2ae3a2fb
JuanParraIV <jmparra1993@gmail.com>
2022-08-30 09:14:04 -0500

    fix: create a paginary

--- components/Paginated/index.tsx
+++ components/Paginated/index.tsx
@@ -1,34 +1,7 @@
 import styles from './Paginated.module.scss'
-function Paginated(/* {
-  setCurrentPage,
-  currentPage,
-  elementsPerPage,
-  totalElements,
-  paginated,
-} */) {
-  /* const pageNumbers = []
-
-  for (
-    let number = 1
-    number <= Math.ceil(totalElements / elementsPerPage)
-    number++
-  ) {
-    pageNumbers.push(number)
-  }
-
-  const forward = (pageNumber) => {
-    setCurrentPage(
-      pageNumber === pageNumbers.length ? pageNumber : pageNumber + 1
-    )
-  }
-
-  const backward = (pageNumber) => {
-    setCurrentPage(pageNumber === 1 ? pageNumber : pageNumber - 1)
-  } */
-
+function Paginated() {
   return (
     <div className={styles['paginated-container']}>
-      {/*    {pageNumbers[0] && ( */}
       <div>
         <label htmlFor=""> 1 of 2 </label>
       </div>
@@ -36,37 +9,10 @@ function Paginated(/* {
       <div>
         <ul className="flex">
           <li className={styles.button} /* onClick={() => backward(currentPage)} */>Antes</li>
-          {/*   {pageNumbers &&
-            pageNumbers.map((number) => {
-              if (currentPage === number)
-                return ( */}
-          <li
-            key={1}
-            /* onClick={() => paginated(number)} */
-          >
-            {1}
-          </li>
-          <li
-            key={1}
-            /* onClick={() => paginated(number)} */
-          >
-            {2}
-          </li>
-          <li
-            key={1}
-            /* onClick={() => paginated(number)} */
-          >
-            {3}
-          </li>
-          {/* ) */}
-          {/*  else
-                return ( */}
-          {/* <StyledLi key={number} onClick={() => paginated(number)}>
-                    {number}
-                    </StyledLi>
-                  ) */}
-          {/*  })} */}
-          <li className={styles.button} /* onClick={() => forward(currentPage)} */>Siguiente</li>
+          <li key={1}> {1}</li>
+          <li key={1}>{2}</li>
+          <li key={1}>{3}</li>
+          <li className={styles.button}>Siguiente</li>
         </ul>
         {/*  )} */}
       </div>
