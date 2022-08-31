db9fb3d1d1575885610922de6cf30fbfa367ab15
JuanParraIV <jmparra1993@gmail.com>
2022-08-31 10:23:31 -0500

    fix:  putting the styles that are in the attached design link

--- components/Paginated/index.tsx
+++ components/Paginated/index.tsx
@@ -2,19 +2,26 @@ import styles from './Paginated.module.scss'
 function Paginated() {
   return (
     <div className={styles['paginated-container']}>
-      <div>
+      <div className={styles['pages-number']}>
         <label htmlFor=""> 1 of 2 </label>
       </div>
 
       <div>
-        <ul className="flex">
-          <li className={styles.button} /* onClick={() => backward(currentPage)} */>Antes</li>
-          <li key={1}> {1}</li>
-          <li key={1}>{2}</li>
-          <li key={1}>{3}</li>
-          <li className={styles.button}>Siguiente</li>
+        <ul>
+          <div className={styles.button}>
+            <span>Antes</span>
+          </div>
+          <div className={styles['pages-list']}>
+            <li key={1}>{1}</li>
+            <li key={1}>{2}</li>
+            <li key={1}>{3}</li>
+            <li key={1}>{4}</li>
+            <li key={1}>{5}</li>
+          </div>
+          <div className={styles.button}>
+            <span>Siguiente</span>
+          </div>
         </ul>
-        {/*  )} */}
       </div>
     </div>
   )
