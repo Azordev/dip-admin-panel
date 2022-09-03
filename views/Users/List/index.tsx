182c953605ad8b546b7fbf7d33f7d530d22484e3
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-09-03 15:35:18 -0400

    feat: added table to show the users

--- views/Users/List/index.tsx
+++ views/Users/List/index.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { FC, useCallback, useMemo, useState } from "react";
 
 import ListHeader from "@/components/ListHeader";
@@ -78,13 +79,63 @@ const UsersList: FC<{
 
   return (
     <div className={styles["userlist-container"]}>
+=======
+import { FC, useMemo } from 'react'
+
+import Switch from '@/components/CustomSwitch'
+import ListHeader from '@/components/ListHeader'
+import Table, { TableData } from '@/components/Table'
+import { User } from '@/services/GraphQL/users/types'
+
+import styles from './List.module.scss'
+
+const UsersList: FC<{ users: User[] }> = ({ users }) => {
+  const headers = ['CÓDIGO', 'Fecha', 'Socio', 'Correo electrónico', 'Contraseña', 'Estado']
+
+  const data: TableData[] = useMemo(() => {
+    return users.map(user => {
+      const name = `${user.memberInfo?.firstNames} ${user.memberInfo?.lastNames}`
+      const Active = () => (
+        <span className={user.isActive ? styles.active : styles.inactive}>{user.isActive ? 'Activo' : 'Inactivo'}</span>
+      )
+      const Actions = () => (
+        <div>
+          <Switch isChecked={user?.isActive ?? false} onChange={value => console.log(value)} />
+          <button>Edit</button>
+        </div>
+      )
+
+      return {
+        id: user.id,
+        items: [
+          user.id,
+          user.createdAt,
+          name,
+          user.memberInfo?.email,
+          '**********',
+          <Active key={`active-${user.id}`} />,
+          <Actions key={`actions-${user.id}`} />,
+        ],
+      }
+    })
+  }, [users])
+
+  return (
+    <div>
+>>>>>>> b371e4e (feat: added table to show the users)
       <ListHeader createText="Añadir socio" createPath="/socios/crear" />
       <div className={styles.container}>
         <Table headers={headers} data={data} />
       </div>
+<<<<<<< HEAD
       {/* TODO: Colocar paginacion */}
     </div>
   );
 };
+=======
+    </div>
+  )
+}
+>>>>>>> b371e4e (feat: added table to show the users)
 
 export default UsersList;
