b371e4e0de63e65c84947d8361cce455e087820c
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-09-03 15:35:18 -0400

    feat: added table to show the users

--- views/Users/List/index.tsx
+++ views/Users/List/index.tsx
@@ -1,27 +1,50 @@
-import { useRouter } from 'next/router'
-import { FC } from 'react'
+import { FC, useMemo } from 'react'
 
+import Switch from '@/components/CustomSwitch'
+import ListHeader from '@/components/ListHeader'
+import Table, { TableData } from '@/components/Table'
 import { User } from '@/services/GraphQL/users/types'
 
+import styles from './List.module.scss'
+
 const UsersList: FC<{ users: User[] }> = ({ users }) => {
-  const { push } = useRouter()
-  return (
-    <>
-      <header>
-        <h1>Usuarios</h1>
-        <button onClick={() => push('/socios/crear')}>Crear Usuario</button>
-      </header>
-      <div>
+  const headers = ['CÓDIGO', 'Fecha', 'Socio', 'Correo electrónico', 'Contraseña', 'Estado']
+
+  const data: TableData[] = useMemo(() => {
+    return users.map(user => {
+      const name = `${user.memberInfo?.firstNames} ${user.memberInfo?.lastNames}`
+      const Active = () => (
+        <span className={user.isActive ? styles.active : styles.inactive}>{user.isActive ? 'Activo' : 'Inactivo'}</span>
+      )
+      const Actions = () => (
         <div>
-          {users.map((user: User) => (
-            <div key={user.id}>
-              <h2>{user.memberCode}</h2>
-              <p>{user.position}</p>
-            </div>
-          ))}
+          <Switch isChecked={user?.isActive ?? false} onChange={value => console.log(value)} />
+          <button>Edit</button>
         </div>
+      )
+
+      return {
+        id: user.id,
+        items: [
+          user.id,
+          user.createdAt,
+          name,
+          user.memberInfo?.email,
+          '**********',
+          <Active key={`active-${user.id}`} />,
+          <Actions key={`actions-${user.id}`} />,
+        ],
+      }
+    })
+  }, [users])
+
+  return (
+    <div>
+      <ListHeader createText="Añadir socio" createPath="/socios/crear" />
+      <div className={styles.container}>
+        <Table headers={headers} data={data} />
       </div>
-    </>
+    </div>
   )
 }
 
