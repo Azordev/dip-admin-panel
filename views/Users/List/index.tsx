120f95b51557e69e3cd43801a31d120ac1f3fca0
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-09-03 15:35:18 -0400

    feat: added table to show the users

--- views/Users/List/index.tsx
+++ views/Users/List/index.tsx
@@ -1,6 +1,8 @@
-import { useRouter } from 'next/router'
-import { FC } from 'react'
+import { FC, useMemo } from 'react'
 
+import Switch from '@/components/CustomSwitch'
+import ListHeader from '@/components/ListHeader'
+import Table, { TableData } from '@/components/Table'
 import { User } from '@/services/GraphQL/users/types'
 
 import styles from './userlist.module.scss'
@@ -10,23 +12,42 @@ const UsersList: FC<{ users: User[]; indexOfFirstPartner: number; indexOfLastPar
   indexOfFirstPartner,
   indexOfLastPartner,
 }) => {
-  const { push } = useRouter()
+  const headers = ['CÓDIGO', 'Fecha', 'Socio', 'Correo electrónico', 'Contraseña', 'Estado']
   const sliceUsers = users.slice(indexOfFirstPartner, indexOfLastPartner)
+
+  const data: TableData[] = useMemo(() => {
+    return sliceUsers.map(user => {
+      const name = `${user.memberInfo?.firstNames} ${user.memberInfo?.lastNames}`
+      const Active = () => (
+        <span className={user.isActive ? styles.active : styles.inactive}>{user.isActive ? 'Activo' : 'Inactivo'}</span>
+      )
+      const Actions = () => (
+        <div>
+          <Switch isChecked={user?.isActive ?? false} onChange={value => console.log(value)} />
+          <button>Edit</button>
+        </div>
+      )
+
+      return {
+        id: user.id,
+        items: [
+          user.id,
+          user.createdAt,
+          name,
+          user.memberInfo?.email,
+          '**********',
+          <Active key={`active-${user.id}`} />,
+          <Actions key={`actions-${user.id}`} />,
+        ],
+      }
+    })
+  }, [sliceUsers])
+
   return (
     <div className={styles['userlist-container']}>
-      <header>
-        <h1>Usuarios</h1>
-        <button onClick={() => push('/socios/crear')}>Crear Usuario</button>
-      </header>
-      <div>
-        <div>
-          {sliceUsers.map((user: User) => (
-            <div key={user.id}>
-              <h2>{user.memberCode}</h2>
-              <p>{user.position}</p>
-            </div>
-          ))}
-        </div>
+      <ListHeader createText="Añadir socio" createPath="/socios/crear" />
+      <div className={styles.container}>
+        <Table headers={headers} data={data} />
       </div>
     </div>
   )
