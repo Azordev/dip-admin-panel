1568c7e8febb6f07a1e8c5266047bc11846ece58
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-09-05 19:42:58 -0400

    feat: added provider's table

--- views/Providers/List/index.tsx
+++ views/Providers/List/index.tsx
@@ -1,22 +1,67 @@
-import Link from 'next/link'
-import { FC } from 'react'
+import { FC, useCallback, useMemo, useState } from 'react'
 
 import ListHeader from '@/components/ListHeader'
+import Table, { TableData } from '@/components/Table'
+import Actions from '@/components/Table/Actions'
 import { Provider } from '@/services/GraphQL/providers/types'
 
-const ProvidersList: FC<{ providers: Provider[] }> = ({ providers }) => (
-  <div>
-    <ListHeader createText="Crear nuevo proveedor" createPath="/proveedores/crear" />
+import styles from './List.module.scss'
 
-    {providers.map(provider => (
-      <Link key={provider.id} href="/proveedores/[id]" as={`/proveedores/${provider.id}`}>
-        <a>
-          <h2>{provider.commercialName}</h2>
-          <p>{provider.salesPhone}</p>
-        </a>
-      </Link>
-    ))}
-  </div>
-)
+const ProvidersList: FC<{ providers: Provider[] }> = ({ providers: dbProviders }) => {
+  const [providers, setProviders] = useState(dbProviders)
+  const headers = ['Rango', 'Fecha', 'Empresa', 'Usuario', 'Contraseña', 'Estado']
+
+  const handleSwitchProvider = useCallback(
+    (value: boolean, providerId: Provider['id']) => {
+      const newProviders = providers.map(provider => {
+        if (provider.id === providerId) {
+          return { ...provider, isActive: value }
+        }
+        return provider
+      })
+
+      setProviders(newProviders)
+    },
+    [providers],
+  )
+
+  const data: TableData[] = useMemo(() => {
+    const Active = ({ active }: { active: boolean }) => {
+      return <span className={active ? styles.active : styles.inactive}>{active ? 'Activo' : 'Inactivo'}</span>
+    }
+
+    return providers.map(provider => {
+      const isActive = Boolean(provider?.isActive)
+      return {
+        id: provider.id,
+        items: [
+          provider.id,
+          provider.createdAt,
+          provider.commercialName,
+          'email@gmail.com',
+          '**********',
+          <Active key={`${provider.id}-active`} active={isActive} />,
+          <Actions
+            key={`${provider.id}-actions`}
+            editLink={`/proveedores/editar/${provider.id}`}
+            showSwitch
+            isCheckedSwitch={isActive}
+            onSwitchChange={value => handleSwitchProvider(value, provider.id)}
+          />,
+        ],
+      }
+    })
+  }, [providers, handleSwitchProvider])
+
+  return (
+    <div className={styles.container}>
+      <ListHeader createText="Crear nuevo proveedor" createPath="/proveedores/crear" />
+
+      <div className={styles['table-container']}>
+        <Table headers={headers} data={data} />
+      </div>
+    </div>
+  )
+}
 
 export default ProvidersList
