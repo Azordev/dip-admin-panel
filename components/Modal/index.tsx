200429e595ecfd3474ffd7024547b276501bb6c1
Eghinner <eghinner@gmail.com>
2022-09-07 12:16:49 -0400

    feat: react modal

--- components/Modal/index.tsx
+++ components/Modal/index.tsx
@@ -1,29 +1,24 @@
 import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 
 export default function Modal(str: string, callback: any) {
-  return Swal.fire({
-    allowOutsideClick: () => !Swal.isLoading(),
+  const MySwal = withReactContent(Swal)
+  return MySwal.fire({
     backdrop: 'rgba(0, 0, 0, 0.7)',
     confirmButtonText: 'Aceptar',
     input: 'text',
-    inputValidator: value => {
-      if (!value) {
-        return 'Ingresa una palabra'
-      } else if (value !== 'ELIMINAR') {
-        return 'Palabra incorrecta'
-      }
-    },
     inputPlaceholder: 'ELIMINAR',
     padding: '50px 120px 40px',
     showCancelButton: true,
     html: '<p>Si desea eliminar este, escriba <span>ELIMINAR</span> en el siguiente recuadro.</p>',
     title: `¿Desea eliminar este ${str}?`,
     width: 874,
+    // eslint-disable-next-line func-names
   }).then(confirmed => {
     if (confirmed.value === 'ELIMINAR') {
       // eslint-disable-next-line promise/no-callback-in-promise
       callback(confirmed.isConfirmed)
-      return Swal.fire('Eliminado!')
+      return MySwal.fire('Eliminado!')
     } else {
       return false
     }
