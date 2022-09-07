b87e39ca85ee32571df298df53029e67c7b32f3d
PrototypeK02 <100804258+PrototypeK02@users.noreply.github.com>
2022-09-07 19:54:10 -0300

    Back button in create provider

--- views/Providers/Create/Form.tsx
+++ views/Providers/Create/Form.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react'
 import { useForm } from 'react-hook-form'
 
+import BackHeader from '@/components/BackHeader'
 import { MutableProviderFormProps, ProviderEditable } from '@/services/GraphQL/providers/types'
 
 const CreateProviderForm: FC<MutableProviderFormProps> = ({ onSubmit, loading }) => {
@@ -13,16 +14,19 @@ const CreateProviderForm: FC<MutableProviderFormProps> = ({ onSubmit, loading })
   const buttonText = loading ? 'Enviando' : 'Enviar'
 
   return (
-    <form onSubmit={submitHandler}>
-      <input type="text" placeholder="Comercial Name" {...register('commercialName', { required: true })} />
-      {errors.commercialName && <span>This field is required</span>}
-      <input type="text" placeholder="WhatsApp Phone" {...register('salesPhone')} />
-      {errors.salesPhone && <span>This field is required</span>}
-      <input type="number" min={0} placeholder="Order Index" {...register('orderIndex')} />
-      <input type="text" placeholder="Plan" {...register('plan')} />
-      <input type="text" placeholder="Logo URL" {...register('logoUrl')} />
-      <button type="submit">{buttonText}</button>
-    </form>
+    <>
+      <BackHeader parent="" />
+      <form onSubmit={submitHandler}>
+        <input type="text" placeholder="Comercial Name" {...register('commercialName', { required: true })} />
+        {errors.commercialName && <span>This field is required</span>}
+        <input type="text" placeholder="WhatsApp Phone" {...register('salesPhone')} />
+        {errors.salesPhone && <span>This field is required</span>}
+        <input type="number" min={0} placeholder="Order Index" {...register('orderIndex')} />
+        <input type="text" placeholder="Plan" {...register('plan')} />
+        <input type="text" placeholder="Logo URL" {...register('logoUrl')} />
+        <button type="submit">{buttonText}</button>
+      </form>
+    </>
   )
 }
 
