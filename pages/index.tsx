12f726d03a1acf291ae0054de5801f8ed867fc69
Eghinner <eghinner@gmail.com>
2022-09-06 20:35:09 -0400

    feat: redirect type user

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,18 @@
-const DIDAdminPanel = () => {
-  return <h1>DID Admin Panel</h1>
-}
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
-export async function getStaticProps() {
-  return {
-    redirect: {
-      permanent: false,
-      destination: '/eventos',
-    },
-  }
+import useAuth from '@/hooks/useAuth'
+
+const DIDAdminPanel = () => {
+  const router = useRouter()
+  const { user } = useAuth()
+  useEffect(() => {
+    if (user?.type === 'ADMIN') {
+      router.push('/eventos')
+    } else if (user?.type === 'PROVIDER') {
+      router.push('/productos')
+    }
+  })
 }
 
 export default DIDAdminPanel
