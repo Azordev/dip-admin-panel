0f184011f769a4fa88426ffd85cbcebed6c77117
PrototypeK02 <100804258+PrototypeK02@users.noreply.github.com>
2022-09-07 19:50:33 -0300

    Back button in create event

--- views/Events/Create/Form.tsx
+++ views/Events/Create/Form.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react'
 import { useForm } from 'react-hook-form'
 
+import BackHeader from '@/components/BackHeader'
 import { EventEditable, MutableEventFormProps } from '@/services/GraphQL/events/types'
 
 const CreateEventForm: FC<MutableEventFormProps> = ({ onSubmit, loading }) => {
@@ -11,18 +12,21 @@ const CreateEventForm: FC<MutableEventFormProps> = ({ onSubmit, loading }) => {
   const buttonText = loading ? 'Enviando' : 'Enviar'
 
   return (
-    <form onSubmit={onSubmit}>
-      <input type="text" placeholder="title" {...register('title', { required: true })} />
-      {errors.title && <small className="text-red-500">{errors.title.message}</small>}
-      <input type="text" placeholder="description" {...register('description')} />
-      <input type="datetime-local" {...register('date', { required: true })} />
-      <input type="file" name="image" accept="image/*" />
-      <select {...register('type', { required: true })}>
-        <option value="ATTENDANCE">Evento</option>
-        <option value="WORKSHOP">Convocatoria</option>
-      </select>
-      <button type="submit">{buttonText}</button>
-    </form>
+    <>
+      <BackHeader parent="" />
+      <form onSubmit={onSubmit}>
+        <input type="text" placeholder="title" {...register('title', { required: true })} />
+        {errors.title && <small className="text-red-500">{errors.title.message}</small>}
+        <input type="text" placeholder="description" {...register('description')} />
+        <input type="datetime-local" {...register('date', { required: true })} />
+        <input type="file" name="image" accept="image/*" />
+        <select {...register('type', { required: true })}>
+          <option value="ATTENDANCE">Evento</option>
+          <option value="WORKSHOP">Convocatoria</option>
+        </select>
+        <button type="submit">{buttonText}</button>
+      </form>
+    </>
   )
 }
 
