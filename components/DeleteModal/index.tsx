315120c4f72248cc23cd55bcb824b6ce205f244b
Eghinner <eghinner@gmail.com>
2022-09-07 12:47:03 -0400

    feat: cancel fire

--- components/DeleteModal/index.tsx
+++ components/DeleteModal/index.tsx
@@ -1,7 +1,7 @@
 import Swal from 'sweetalert2'
 import withReactContent from 'sweetalert2-react-content'
 
-export default function Modal(str: string, callback: any) {
+export default function DeleteModal(itemName: string, confirmButtonText: any) {
   const MySwal = withReactContent(Swal)
   return MySwal.fire({
     backdrop: 'rgba(0, 0, 0, 0.7)',
@@ -10,17 +10,16 @@ export default function Modal(str: string, callback: any) {
     inputPlaceholder: 'ELIMINAR',
     padding: '50px 120px 40px',
     showCancelButton: true,
-    html: '<p>Si desea eliminar este, escriba <span>ELIMINAR</span> en el siguiente recuadro.</p>',
-    title: `¿Desea eliminar este ${str}?`,
+    html: '<p>Si desea eliminar este,' + itemName + ' escriba <span>ELIMINAR</span> en el siguiente recuadro.</p>',
+    title: `¿Desea eliminar este ${itemName}?`,
     width: 874,
-    // eslint-disable-next-line func-names
   }).then(confirmed => {
     if (confirmed.value === 'ELIMINAR') {
       // eslint-disable-next-line promise/no-callback-in-promise
-      callback(confirmed.isConfirmed)
-      return MySwal.fire('Eliminado!')
+      confirmButtonText(confirmed.isConfirmed)
+      return MySwal.fire('Eliminado!', '', 'success')
     } else {
-      return false
+      return MySwal.fire('Cancelado', '', 'info')
     }
   })
 }
--- views/Events/Edit/index.tsx
+++ views/Events/Edit/index.tsx
@@ -6,7 +6,7 @@ import { SubmitHandler, useForm } from 'react-hook-form'
 import Button from '@/components/Button'
 import CustomInput from '@/components/CustomInput'
 import CustomSwitch from '@/components/CustomSwitch'
-import Modal from '@/components/Modal'
+import DeleteModal from '@/components/DeleteModal'
 import { EventEditable, MutableEventFormProps } from '@/services/GraphQL/events/types'
 import styles from '@/styles/EditEvent.module.scss'
 import Icons8 from '@/views/Shared/Icons8'
@@ -81,7 +81,7 @@ const EditEventForm: FC<MutableEventFormProps> = ({ onSubmit, loading, originalD
   }
 
   const showModal = () => {
-    Modal('evento', (confirmed: boolean) => {
+    DeleteModal('evento', (confirmed: boolean) => {
       if (confirmed) {
         // Add petition Delete
       }
