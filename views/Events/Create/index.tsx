3f12c76a5e2733f878c5c1d337b3cbf8588f0e71
Cesar Leon <100804258+PrototypeK02@users.noreply.github.com>
2022-09-10 06:52:16 -0300

    Back button in create event (#188)
    * Back button in create event
    
    * fix: move back button component
    
    * fix: back button behavior
    
    Co-authored-by: Israel-Laguan <israellaguan@gmail.com>

--- views/Events/Create/index.tsx
+++ views/Events/Create/index.tsx
@@ -1,16 +1,18 @@
 import { FC } from 'react'
 
+import BackHeader from '@/components/BackHeader'
 import { MutableEventFormProps } from '@/services/GraphQL/events/types'
 import styles from '@/styles/Home.module.css'
 
 import CreateEventForm from './Form'
 
 const CreateEventLayout: FC<MutableEventFormProps> = ({ onSubmit, loading }) => (
-  <div className={styles.container}>
-    <h1 className={styles.title}>Create Event</h1>
-
-    <CreateEventForm onSubmit={onSubmit} loading={loading} />
-  </div>
+  <>
+    <BackHeader to="/eventos" />
+    <div className={styles.container}>
+      <CreateEventForm onSubmit={onSubmit} loading={loading} />
+    </div>
+  </>
 )
 
 export default CreateEventLayout
