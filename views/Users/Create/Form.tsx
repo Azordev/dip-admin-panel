ce061f64170885696ae8e6569f6758dd19344718
JuanParraIV <jmparra1993@gmail.com>
2022-09-11 21:49:17 -0500

    fix: corregir diseño añadir socio (#194)
    * fix: corregir diseño añadir socio
    
    * fix: corregir diseño añadir socio
    
    * fix: correct indent
    
    * fix: removing yarn.lock
    
    * fix: restore lockfile
    
    * fix: lint and style
    
    * fix: back button to correct place
    
    Co-authored-by: Israel-Laguan <israellaguan@gmail.com>

--- views/Users/Create/Form.tsx
+++ views/Users/Create/Form.tsx
@@ -1,82 +1,110 @@
+import { useRouter } from 'next/router'
 import { FC } from 'react'
 import { useForm } from 'react-hook-form'
 
+import Button from '@/components/Button'
+import style from '@/components/Button/Button.module.scss'
+import DeleteModal from '@/components/DeleteModal'
 import { MutableUserFormProps, UserEditable } from '@/services/GraphQL/users/types'
+
+import styles from './Form.module.scss'
+
 const CreateUserForm: FC<MutableUserFormProps> = ({ onSubmit, loading }) => {
+  const showModal = () => {
+    DeleteModal('socio', (confirmed: boolean) => {
+      if (confirmed) {
+        console.log('abrió modal')
+      }
+    })
+  }
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<UserEditable>()
   const submitHandler = handleSubmit(onSubmit)
-  const buttonText = loading ? 'Enviando' : 'Enviar'
-
+  const buttonText = loading ? 'Añadiendo' : 'Añadir'
   return (
     <>
-      <form className="form" onSubmit={submitHandler}>
-        <label className="text-size label" htmlFor="namePartner">
-          Nombre del Socio
-        </label>
-        <input
-          className="input-member font-visby"
-          id="namePartner"
-          type="text"
-          placeholder="Escribe el nombre del socio"
-          {...register('namePartner', { required: true })}
-        />
-        {errors.namePartner && <small className="text-red-100">{errors.namePartner.message}</small>}
-
-        <label className="text-size label" htmlFor="memberCode">
-          Código del Socio
-        </label>
-        <input
-          className="input-member font-visby"
-          id="memberCode"
-          type="text"
-          placeholder="Escribe el código del socio"
-          {...register('memberCode', { required: true })}
-        />
-        {errors.memberCode && <small className="text-red-100">{errors.memberCode.message}</small>}
-
-        <label className="text-size label" htmlFor="startDate">
-          Fecha de Inicio
-        </label>
-        <input className="input-member" id="startDate" type="date" {...register('startDate', { required: true })} />
-        {errors.startDate && <small className="text-red-100">{errors.startDate.message}</small>}
-
-        <label className="text-size label" htmlFor="email">
-          Correo electrónico
-        </label>
-        <input
-          className="input-member font-visby"
-          id="email"
-          type="text"
-          placeholder="Inserte el correo electrónico"
-          {...register('email', { required: true })}
-        />
-        {errors.email && <small className="text-red-500">{errors.email.message}</small>}
-
-        <label className="text-size label" htmlFor="password">
-          Contraseña
-        </label>
-        <input
-          className="input-member font-visby"
-          id="password"
-          type="password"
-          placeholder="Inserte la contraseña del socio"
-          {...register('password')}
-        />
-        {errors.password && <small className="text-red-500">{errors.password.message}</small>}
-
-        <div className="button-container">
-          <button className="add text-size" type="submit">
+      <form className={styles.form} onSubmit={submitHandler}>
+        <section className={styles.section}>
+          <div className={styles['input-section']}>
+            <div className={styles['custom-input']}>
+              <label className={styles['label-input']} htmlFor="namePartner">
+                Nombre del Socio
+              </label>
+              <input
+                className={styles.input}
+                id="namePartner"
+                type="text"
+                placeholder="Escribe el nombre del socio"
+                {...register('namePartner', { required: true })}
+              />
+              {errors.namePartner && <small className={styles['error-message']}>{errors.namePartner.message}</small>}
+            </div>
+            <div className={styles['custom-input']}>
+              <label className={styles['label-input']} htmlFor="memberCode">
+                Código del Socio
+              </label>
+              <input
+                className={styles.input}
+                id="memberCode"
+                type="text"
+                placeholder="Escribe el código del socio"
+                {...register('memberCode', { required: true })}
+              />
+              {errors.memberCode && <small className={styles['error-message']}>{errors.memberCode.message}</small>}
+            </div>
+            <div className={styles['custom-input']}>
+              <label className={styles['label-input']} htmlFor="startDate">
+                Fecha de Inicio
+              </label>
+              <input
+                className={styles.input}
+                id="startDate"
+                type="date"
+                placeholder="Escribe el código del socio"
+                {...register('startDate', { required: true })}
+              />
+              {errors.startDate && <small className={styles['error-message']}>{errors.startDate.message}</small>}
+            </div>
+            <div className={styles['custom-input']}>
+              <label className={styles['label-input']} htmlFor="email">
+                Correo electrónico
+              </label>
+              <input
+                className={styles.input}
+                id="email"
+                type="text"
+                placeholder="Inserte el correo electrónico"
+                {...register('email', { required: true })}
+              />
+              {errors.email && <small className={styles['error-message']}>{errors.email.message}</small>}
+            </div>
+            <div className={styles['custom-input']}>
+              <label className={styles['label-input']} htmlFor="password">
+                Contraseña
+              </label>
+              <input
+                className={styles.input}
+                id="password"
+                type="password"
+                placeholder="Inserte la contraseña del socio"
+                {...register('password', { required: true })}
+              />
+              {errors.password && <small className={styles['error-message']}>{errors.password.message}</small>}
+            </div>
+          </div>
+        </section>
+        <section className={style['buttons-container']}>
+          <Button iconName="" className={style['button-save']} type="submit">
             {buttonText}
-          </button>
-
-          <button className="delete text-size" type="submit">
+          </Button>
+          <Button iconName="" className={style['button-delete']} onClick={showModal}>
             Eliminar
-          </button>
-        </div>
+          </Button>
+          {/* TODO: implementar funcionalidad asociada a este botón */}
+        </section>
       </form>
     </>
   )
