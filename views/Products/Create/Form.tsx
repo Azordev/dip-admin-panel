8cbf141c14a1d1b7d5c531d05a1abe485bfffddd
PrototypeK02 <100804258+PrototypeK02@users.noreply.github.com>
2022-09-13 18:27:28 -0300

    cambios en el form

--- views/Products/Create/Form.tsx
+++ views/Products/Create/Form.tsx
@@ -1,13 +1,15 @@
-import { FC } from 'react'
+import axios from 'axios'
+import Image from 'next/image'
+import { ChangeEvent, FC, useState } from 'react'
 import { useForm } from 'react-hook-form'
 
 import { MutableProductFormProps, ProductEditable } from '@/services/GraphQL/products/types'
-
-import { AddImageSVG } from './AddImageSVG'
+import stylesInput from '@/styles/EditEvent.module.scss'
 
 import styles from './SaveorDelete.module.scss'
-
 const CreateProductForm: FC<MutableProductFormProps> = ({ onSubmit, loading }) => {
+  const [imageFile, setImageFile] = useState<File | null>(null)
+  const [imageUrl, setImageUrl] = useState<string | null>(null)
   const {
     register,
     handleSubmit,
@@ -15,6 +17,19 @@ const CreateProductForm: FC<MutableProductFormProps> = ({ onSubmit, loading }) =
     formState: { errors },
   } = useForm<ProductEditable>()
   const submitHandler = handleSubmit(onSubmit)
+  const handleFile = (evt: ChangeEvent<HTMLInputElement>) => {
+    const file = evt.target.files?.[0]
+    if (file?.type.includes('image')) {
+      setImageFile(file)
+      const reader = new FileReader()
+      reader.onload = (e: ProgressEvent<FileReader>) => {
+        if (e.target) {
+          setImageUrl(e.target.result as string)
+        }
+      }
+      reader.readAsDataURL(file)
+    }
+  }
 
   return (
     <form className="form-product" onSubmit={submitHandler}>
@@ -26,8 +41,9 @@ const CreateProductForm: FC<MutableProductFormProps> = ({ onSubmit, loading }) =
         id="name"
         type="text"
         placeholder="Escriba el nombre del evento..."
-        {...register('name', { required: true })}
+        {...register('name', { required: { value: true, message: 'El campo no puede estar vacio' } })}
       />
+      {errors.name && <small className="text-red-500">{errors.name.message}</small>}
 
       <label className="text-size label" htmlFor="basePriceSol">
         Precio del producto
@@ -38,11 +54,12 @@ const CreateProductForm: FC<MutableProductFormProps> = ({ onSubmit, loading }) =
         <input
           className="input-product font-visby"
           type="text"
+          id="basePriceSol"
           placeholder="00.00"
-          {...register('basePriceSol', { required: true })}
+          {...register('name', { required: { value: true, message: 'Debe colocar un precio' } })}
         />
-        {errors.basePriceSol && <small className="text-red-500">{errors.basePriceSol.message}</small>}
       </div>
+      {errors.basePriceSol && <small className="text-red-500">{errors.basePriceSol.message}</small>}
 
       <label className="text-size label" htmlFor="description">
         Descripción del producto
@@ -51,21 +68,42 @@ const CreateProductForm: FC<MutableProductFormProps> = ({ onSubmit, loading }) =
         className="textarea font-visby"
         id="description"
         placeholder="Escribe aquí..."
-        {...register('description', { required: true })}
+        {...register('description', { required: { value: true, message: 'El campo no puede estar vacio' } })}
       />
       {errors.description && <small className="text-red-500">{errors.description.message}</small>}
 
       <label className="text-size label">Añadir imagen del producto</label>
-      <label
-        className="image font-visby"
-        // {...register('imageUrl', { required: true })}
-      >
-        {AddImageSVG}
-        Añadir imagen
-      </label>
+      <div className={stylesInput['image-section']}>
+        <div className={stylesInput['container-input']}>
+          <input
+            id="image-file"
+            type="file"
+            accept="image/*"
+            className={stylesInput['input-file']}
+            {...register('imageUrl')}
+            onChange={handleFile}
+          />
+          <label htmlFor="image-file" className={stylesInput.image}>
+            <figure>
+              <Image
+                width={imageUrl ? 300 : 40}
+                height={imageUrl ? 200 : 40}
+                objectFit="contain"
+                src={imageUrl || 'https://img.icons8.com/ios/100/image.png'}
+                alt="Imagen del evento"
+              />
+            </figure>
+            <span className={stylesInput.label}>{imageFile?.name ? 'Cambiar imagen' : 'Añadir imagen'}</span>
+          </label>
+        </div>
+      </div>
       {errors.imageUrl && <small className="text-red-500">{errors.imageUrl.message}</small>}
 
-      <input type="text" placeholder="base_price_sol" {...register('basePriceSol', { required: true })} />
+      <input
+        type="text"
+        placeholder="base_price_sol"
+        {...register('basePriceSol', { required: { value: true, message: 'Debe colocar un precio' } })}
+      />
       {errors.basePriceSol && <small className="text-red-500">{errors.basePriceSol.message}</small>}
       <button className={styles.save} type="submit">
         {loading ? 'Guardando' : 'Guardar'}
