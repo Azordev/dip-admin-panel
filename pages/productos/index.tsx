4945788772bfa96e4fe44eaa379a4d3acaff0865
victor-heliomar <victordev2002@gmail.com>
2022-09-14 17:11:20 -0400

    fix: added loading message

--- pages/productos/index.tsx
+++ pages/productos/index.tsx
@@ -6,11 +6,20 @@ import EmptyList from '@/components/EmptyList'
 import ListHeader from '@/components/ListHeader'
 import useAuth from '@/hooks/useAuth'
 import { Product } from '@/services/GraphQL/products/types'
+import styles from '@/styles/Products.module.scss'
 import ProductList from '@/views/Products/List'
 import ClientOnly from '@/views/Shared/ClientOnly'
 
+const showProductList = (products: Product[]) =>
+  products?.length > 0 ? (
+    <ProductList products={products} />
+  ) : (
+    <EmptyList className={styles['center-text']} text="No hay productos" />
+  )
+
 const Products: NextPage = () => {
   const [products, setProducts] = useState<Product[]>([])
+  const [isLoading, setIsLoading] = useState<Boolean>(true)
   const { user } = useAuth()
 
   useEffect(() => {
@@ -23,6 +32,7 @@ const Products: NextPage = () => {
         },
       })
       setProducts(data.products)
+      setIsLoading(false)
     }
 
     getProducts()
@@ -37,7 +47,7 @@ const Products: NextPage = () => {
           logoUrl={user?.providerInfo.logoUrl}
           altLogo={user?.providerInfo.commercialName}
         />
-        {!products || products.length < 1 ? <EmptyList text="No hay productos" /> : <ProductList products={products} />}
+        {isLoading ? <div className={styles['center-text']}>Cargando...</div> : showProductList(products)}
       </>
     </ClientOnly>
   )
