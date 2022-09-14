714969f5ff3c3c9705ffb96947841de827e6295d
Israel-Laguan <israellaguan@gmail.com>
2022-09-14 10:15:13 -0500

    feat: improve SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ApolloProvider } from '@apollo/client'
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { ToastContainer, Zoom } from 'react-toastify'
 
@@ -15,6 +16,31 @@ const DIDAdminPanel = ({ Component, pageProps: { ...pageProps } }: AppProps) => 
 
   return (
     <ErrorBoundary>
+      <Head>
+        <title>DID Panel Administrativo</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:title" name="title" content="DID Panel Administrativo" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="DID Panel" />
+        <meta
+          property="og:image"
+          content="https://static.wixstatic.com/media/539145_8d634facac56459abe42ab5d47fa4f74~mv2.jpg/v1/fill/w_2500,h_1668,al_c/539145_8d634facac56459abe42ab5d47fa4f74~mv2.jpg"
+        />
+        <meta
+          property="og:description"
+          name="description"
+          content="Somos la Asociación de Diseñadores de Interiores del Perú. Unidos para promover el interiorismo peruano, basado en nuestra creatividad y cultura."
+        />
+        <meta
+          name="keywords"
+          content="peru, design, diseñadores, Diseño de interiores, Asociación Perú, Interior Designs Peru"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="Spanish" />
+        <meta name="revisit-after" content="45 days" />
+        <meta name="author" content="Israel Antonio Rosales Laguan" />
+      </Head>
       <ApolloProvider client={client}>
         {pathname === '/ingresar' ? (
           <Component {...pageProps} />
