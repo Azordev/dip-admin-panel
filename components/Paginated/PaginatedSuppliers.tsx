11a45493b0da6060bf62e157a111463312b6d4bc
Eghinner <eghinner@gmail.com>
2022-09-14 08:54:28 -0400

    fix: paginate styles

--- components/Paginated/PaginatedSuppliers.tsx
+++ components/Paginated/PaginatedSuppliers.tsx
@@ -26,46 +26,44 @@ const PaginatedSuppliers: FC<PaginatedSuppliersProps> = ({
   const pageNumbers = Array.from({ length: pageNumber }, (_, index) => index + 1)
   const slicedProviders = providers.slice(indexOfFirstProvider, indexOfLastProvider)
   return (
-    <div className={styles.container}>
-      <div className={styles['paginated-container']}>
-        <div className={styles['pages-number']}>
-          <label>
-            {slicedProviders.length * CurrentPage} of {totalProvidersLength}
-          </label>
-        </div>
-        <ul>
-          <div
-            className={styles.button}
-            onClick={() => {
-              if (CurrentPage !== 1) setCurrentPage(CurrentPage - 1)
-            }}
-          >
-            <span>Antes</span>
-          </div>
-          <div className={styles['pages-list']}>
-            {pageNumbers &&
-              pageNumbers.map(page =>
-                CurrentPage === page ? (
-                  <li className={styles.active} key={page} onClick={() => setCurrentPage(page)}>
-                    {page}
-                  </li>
-                ) : (
-                  <li key={page} onClick={() => setCurrentPage(page)}>
-                    {page}
-                  </li>
-                ),
-              )}
-          </div>
-          <div
-            className={styles.button}
-            onClick={() => {
-              if (CurrentPage !== pageNumber) setCurrentPage(CurrentPage + 1)
-            }}
-          >
-            <span>Siguiente</span>
-          </div>
-        </ul>
+    <div className={styles['paginated-container']}>
+      <div className={styles['pages-number']}>
+        <label>
+          {slicedProviders.length * CurrentPage} of {totalProvidersLength}
+        </label>
       </div>
+      <ul>
+        <div
+          className={styles.button}
+          onClick={() => {
+            if (CurrentPage !== 1) setCurrentPage(CurrentPage - 1)
+          }}
+        >
+          <span>Antes</span>
+        </div>
+        <div className={styles['pages-list']}>
+          {pageNumbers &&
+            pageNumbers.map(page =>
+              CurrentPage === page ? (
+                <li className={styles.active} key={page} onClick={() => setCurrentPage(page)}>
+                  {page}
+                </li>
+              ) : (
+                <li key={page} onClick={() => setCurrentPage(page)}>
+                  {page}
+                </li>
+              ),
+            )}
+        </div>
+        <div
+          className={styles.button}
+          onClick={() => {
+            if (CurrentPage !== pageNumber) setCurrentPage(CurrentPage + 1)
+          }}
+        >
+          <span>Siguiente</span>
+        </div>
+      </ul>
     </div>
   )
 }
