c7f0984f59bae705cc72f0ccc1a21be0932ff21d
Eghinner <eghinner@gmail.com>
2022-09-15 11:52:33 -0400

    feat: no attendee & header attendee

--- containers/Asistentes/AttendeesContainer.tsx
+++ containers/Asistentes/AttendeesContainer.tsx
@@ -1,19 +1,24 @@
 import { FC } from 'react'
 
 import BackHeader from '@/components/BackHeader'
-import styles from '@/components/Paginated/Paginated.module.scss'
+import EmptyList from '@/components/EmptyList'
+import { Event } from '@/services/GraphQL/events/types'
 import { Attendee } from '@/services/GraphQL/inscriptions/types'
 import AttendeesList from '@/views/Attendees/List'
 
 interface PaginatedProps {
   attendees: Attendee[]
+  event: Event
 }
 
-const AttendeesContainers: FC<PaginatedProps> = ({ attendees }) => (
-  <div className={styles.relative}>
-    <BackHeader />
-    <AttendeesList attendees={attendees} />
-  </div>
+const AttendeesContainers: FC<PaginatedProps> = ({ attendees, event }) => (
+  <>
+    <BackHeader commercialName={event?.title} parentImageUrl={event?.imageUrl} />
+    {attendees?.length > 0 ? (
+      <AttendeesList attendees={attendees} />
+    ) : (
+      <EmptyList className="center-text" text="No se encuentran asistentes inscritos hasta el momento" />
+    )}
+  </>
 )
-
 export default AttendeesContainers
