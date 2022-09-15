965330e8e6d79a599250859ef0b4c968b6a04385
victor-heliomar <victordev2002@gmail.com>
2022-09-15 10:13:31 -0400

    fix: centered all loading text in the admin panel

--- pages/productos/index.tsx
+++ pages/productos/index.tsx
@@ -4,9 +4,9 @@ import { useEffect, useState } from 'react'
 
 import EmptyList from '@/components/EmptyList'
 import ListHeader from '@/components/ListHeader'
+import Loading from '@/components/Loading'
 import useAuth from '@/hooks/useAuth'
 import { Product } from '@/services/GraphQL/products/types'
-import styles from '@/styles/Products.module.scss'
 import ProductList from '@/views/Products/List'
 import ClientOnly from '@/views/Shared/ClientOnly'
 
@@ -14,7 +14,7 @@ const showProductList = (products: Product[]) =>
   products?.length > 0 ? (
     <ProductList products={products} />
   ) : (
-    <EmptyList className={styles['center-text']} text="No hay productos" />
+    <EmptyList className="center-text" text="No hay productos" />
   )
 
 const Products: NextPage = () => {
@@ -47,7 +47,7 @@ const Products: NextPage = () => {
           logoUrl={user?.providerInfo.logoUrl}
           altLogo={user?.providerInfo.commercialName}
         />
-        {isLoading ? <div className={styles['center-text']}>Cargando...</div> : showProductList(products)}
+        {isLoading ? <Loading className="center-text" /> : showProductList(products)}
       </>
     </ClientOnly>
   )
