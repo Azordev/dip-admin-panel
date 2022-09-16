0f5d02e07306c95b114e4206df458d43762cf401
Eduardo José Quispe Huancahuari <eduardo.quispe@trade.ec>
2022-09-16 16:28:41 -0500

    chore(product-list): format product list component

--- views/Products/List/index.tsx
+++ views/Products/List/index.tsx
@@ -3,21 +3,14 @@ import { FC } from 'react'
 import ProductItem from '@/components/ProductItem'
 import { Product } from '@/services/GraphQL/products/types'
 import styles from '@/styles/Products.module.scss'
-
-interface ProductListProps {
-  products: Product[]
-}
-
-const ProductList: FC<ProductListProps> = ({ products }) => {
-  return (
-    <div className={styles.container}>
-      <div className={styles['products-list']}>
-        {products.map((product: Product) => (
-          <ProductItem key={`product-${product.id}`} product={product} />
-        ))}
-      </div>
+const ProductList: FC<{ products: Product[] }> = ({ products }) => (
+  <div className={styles.container}>
+    <div className={styles['products-list']}>
+      {products.map((product: Product) => (
+        <ProductItem key={`product-${product.id}`} product={product} />
+      ))}
     </div>
-  )
-}
+  </div>
+)
 
 export default ProductList
