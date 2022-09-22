6af1fa8e22c1abbbf1ab303a3d6e293ab1f99d0b
Emmanuel Azocar <azocarmel@gmail.com>
2022-09-21 22:28:05 -0400

    feat: added delete method

--- pages/api/providers/[id].ts
+++ pages/api/providers/[id].ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-import { toggleProvider, updateProvider } from '../../../controllers/providers'
+import { deleteProvider, toggleProvider, updateProvider } from '../../../controllers/providers'
 
 export const config = {
   api: { bodyParser: false },
@@ -9,4 +9,5 @@ export const config = {
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'PATCH') toggleProvider(req, res)
   if (req.method === 'PUT') updateProvider(req, res)
+  if (req.method === 'DELETE') deleteProvider(req, res)
 }
