70f51b8a23b2b6621deb17d7e41f67794971f8d9
Eghinner <eghinner@gmail.com>
2022-12-16 09:13:21 -0400

    fix: cors validation

--- pages/api/members/index.ts
+++ pages/api/members/index.ts
@@ -1,7 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import NextCors from 'nextjs-cors'
 
 import { createMember } from '../../../controllers/members'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  await NextCors(req, res, {
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    origin: '*',
+    optionsSuccessStatus: 200,
+  })
   if (req.method === 'POST') createMember(req, res)
 }
