fb7739adf2cf1e5d99b463260d63fb04ca0c707c
Eghinner <eghinner@gmail.com>
2023-01-05 18:35:12 -0400

    feat: auth subscriptions from did-app

--- pages/api/subscriptions/index.ts
+++ pages/api/subscriptions/index.ts
@@ -1,7 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import NextCors from 'nextjs-cors'
 
 import { createSubscription } from '../../../controllers/subscriptions'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  await NextCors(req, res, {
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    origin: '*',
+    optionsSuccessStatus: 200,
+  })
   if (req.method === 'POST') createSubscription(req, res)
 }
