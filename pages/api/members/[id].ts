526e2a59be46ecfaf1ff1073c29778c6a70ae943
Eghinner <eghinner@gmail.com>
2023-01-30 09:47:01 -0400

    fix: prettier

--- pages/api/members/[id].ts
+++ pages/api/members/[id].ts
@@ -1,19 +1,16 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import NextCors from "nextjs-cors";
+import type { NextApiRequest, NextApiResponse } from 'next'
+import NextCors from 'nextjs-cors'
 
-import { deleteMember, toggleMember, updateMember } from "controllers/members";
+import { deleteMember, toggleMember, updateMember } from 'controllers/members'
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   await NextCors(req, res, {
-    methods: ["DELETE"],
-    origin: "*",
+    methods: ['DELETE'],
+    origin: '*',
     optionsSuccessStatus: 200,
-  });
+  })
 
-  if (req.method === "PATCH") toggleMember(req, res);
-  if (req.method === "PUT") updateMember(req, res);
-  if (req.method === "DELETE") deleteMember(req, res);
+  if (req.method === 'PATCH') toggleMember(req, res)
+  if (req.method === 'PUT') updateMember(req, res)
+  if (req.method === 'DELETE') deleteMember(req, res)
 }
