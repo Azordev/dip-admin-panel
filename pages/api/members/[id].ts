c059bc7078bcbde9afbec72bcc2b824cf076f7c9
Eghinner <eghinner@gmail.com>
2023-01-30 09:42:54 -0400

    fix: cors permission to delete member

--- pages/api/members/[id].ts
+++ pages/api/members/[id].ts
@@ -1,9 +1,19 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import NextCors from "nextjs-cors";
 
-import { deleteMember, toggleMember, updateMember } from 'controllers/members'
+import { deleteMember, toggleMember, updateMember } from "controllers/members";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'PATCH') toggleMember(req, res)
-  if (req.method === 'PUT') updateMember(req, res)
-  if (req.method === 'DELETE') deleteMember(req, res)
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await NextCors(req, res, {
+    methods: ["DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200,
+  });
+
+  if (req.method === "PATCH") toggleMember(req, res);
+  if (req.method === "PUT") updateMember(req, res);
+  if (req.method === "DELETE") deleteMember(req, res);
 }
